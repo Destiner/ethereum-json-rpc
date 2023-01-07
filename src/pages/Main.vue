7a6c2e7c452b74fdaf2fe0b7ac14290581bd0fe6
Timur Badretdinov <destinerx@gmail.com>
2023-01-08 01:59:01 +0400

    Draft Main page layout

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,28 +1,17 @@
 <template>
-  <div>
-    <h1>Main page</h1>
-    <Banner />
-    <div>Latest block on Ethereum: {{ block }}</div>
-  </div>
+  <header>
+    <div>Eth API</div>
+  </header>
+  <main>
+    <section class="methods">
+      <input />
+      <div>
+        <div>eth_blockNumber</div>
+        <div>eth_getBlockByNumber</div>
+        <div>eth_getBlockByHash</div>
+      </div>
+    </section>
+    <section class="params"></section>
+    <section class="execution"></section>
+  </main>
 </template>
-
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
-
-import Banner from '@/components/BannerBase.vue';
-
-const block = ref('0');
-
-async function getLatestBlock(): Promise<string> {
-  const timestamp = Math.floor(Date.now() / 1000);
-  const url = `https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=YourApiKeyToken`;
-  const data = await fetch(url);
-  const json = await data.json();
-  const block = json.result;
-  return block;
-}
-
-onMounted(async () => {
-  block.value = await getLatestBlock();
-});
-</script>
