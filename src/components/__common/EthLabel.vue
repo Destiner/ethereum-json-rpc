041f06fe501fb42edfcb42f2b79d1234f7af1773
Timur Badretdinov <destinerx@gmail.com>
2023-01-14 14:13:02 +0400

    Add EthLabel component

---
+++ src/components/__common/EthLabel.vue
@@ -0,0 +1,18 @@
+<template>
+  <label :for="target">{{ value }}</label>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  value: string;
+  target?: string;
+}>();
+</script>
+
+<style scoped>
+label {
+  color: var(--color-text-secondary);
+  font-size: var(--font-size-small);
+  text-transform: uppercase;
+}
+</style>
