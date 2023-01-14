190b1e11185d3ce51b8405712851604f6dd94938
Timur Badretdinov <destinerx@gmail.com>
2023-01-14 13:52:53 +0400

    Add ChevronDown icon component

---
+++ src/components/__common/icon/ChevronDown.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="m6 9l6 6l6-6"
+    />
+  </svg>
+</template>
