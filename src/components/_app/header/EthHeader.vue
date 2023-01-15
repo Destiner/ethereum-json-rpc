b810ff636ca1f208d1b87456851a86face15540a
Timur Badretdinov <destinerx@gmail.com>
2023-01-15 12:29:03 +0400

    Use ProviderSelector in EthHeader

--- src/components/_app/header/EthHeader.vue
+++ src/components/_app/header/EthHeader.vue
@@ -1,16 +1,25 @@
 <template>
   <header>
-    <div>Ethereum JSON-RPC</div>
+    <div class="label">Ethereum JSON-RPC</div>
+    <ProviderSelector />
   </header>
 </template>
+
+<script setup lang="ts">
+import ProviderSelector from './ProviderSelector.vue';
+</script>
 
 <style scoped>
 header {
   display: flex;
   align-items: center;
+  justify-content: space-between;
   height: 64px;
   padding: 0 24px;
   border-bottom: 1px solid var(--color-border-primary);
+}
+
+.label {
   font-weight: bold;
 }
 </style>
