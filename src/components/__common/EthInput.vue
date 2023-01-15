71bfff7bfc5287c85fbfbd1ed837b1bada5b54b8
Timur Badretdinov <destinerx@gmail.com>
2023-01-15 05:05:14 +0400

    Label support for EthInput

--- src/components/__common/EthInput.vue
+++ src/components/__common/EthInput.vue
@@ -1,18 +1,29 @@
 <template>
-  <input
-    ref="el"
-    type="text"
-    :value="modelValue"
-    @input="handleInput"
-  />
+  <div class="input">
+    <EthLabel
+      v-if="label"
+      :value="label"
+      :for="id"
+    />
+    <input
+      :id="id"
+      ref="el"
+      type="text"
+      :value="modelValue"
+      @input="handleInput"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useActiveElement } from '@vueuse/core';
 import { computed, ref } from 'vue';
 
+import EthLabel from './EthLabel.vue';
+
 defineProps<{
   modelValue: string;
+  label?: string;
 }>();
 
 const emit = defineEmits<{
@@ -23,6 +34,7 @@ const activeElement = useActiveElement();
 
 const el = ref<HTMLInputElement | null>(null);
 
+const id = computed(() => `input-${Math.random().toString().substring(2)}`);
 const isActive = computed(() => activeElement.value === el.value);
 
 function focus(): void {
@@ -43,7 +55,14 @@ defineExpose({
 </script>
 
 <style scoped>
+.input {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-small);
+}
+
 input {
+  width: 100%;
   margin: 0;
   padding: 4px 8px;
   border: 1px solid var(--color-border-primary);
