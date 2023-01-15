bc78798de5f9dd8bba7ee66b94eb369b7d4d24ee
Timur Badretdinov <destinerx@gmail.com>
2023-01-15 07:04:32 +0400

    Add "required" prop to EthLabel

--- src/components/__common/EthLabel.vue
+++ src/components/__common/EthLabel.vue
@@ -1,10 +1,19 @@
 <template>
-  <label :for="target">{{ value }}</label>
+  <label :for="target">
+    {{ value }}
+    <span
+      v-if="required"
+      class="required"
+    >
+      *
+    </span>
+  </label>
 </template>
 
 <script setup lang="ts">
 defineProps<{
   value: string;
+  required?: boolean;
   target?: string;
 }>();
 </script>
@@ -14,5 +23,9 @@ label {
   color: var(--color-text-secondary);
   font-size: var(--font-size-small);
   text-transform: uppercase;
+}
+
+.required {
+  color: var(--color-error);
 }
 </style>
