7349d99a86dc50a44685a5851f598ae0e0125a82
Timur Badretdinov <destinerx@gmail.com>
2023-01-18 17:11:00 +0400

    Add EthRadio component

---
+++ src/components/__common/EthRadio.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="radio">
+    <EthLabel
+      v-if="label"
+      :value="label"
+      :target="id"
+      :disabled="disabled"
+    />
+    <RadioGroup
+      :model-value="selectedOption"
+      @update:model-value="handleUpdate"
+    >
+      <RadioGroupLabel class="caption">Select an option</RadioGroupLabel>
+      <div class="list">
+        <RadioGroupOption
+          v-for="option in options"
+          v-slot="{ active }"
+          :key="option.value"
+          as="template"
+          :value="option.value"
+        >
+          <div
+            :class="{ active, checked: option.value === modelValue }"
+            class="option"
+          >
+            <RadioGroupLabel
+              as="p"
+              :class="{ checked: option.value === modelValue }"
+              class="label"
+            >
+              {{ option.label }}
+            </RadioGroupLabel>
+          </div>
+        </RadioGroupOption>
+      </div>
+    </RadioGroup>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue';
+import { computed } from 'vue';
+
+import EthLabel from './EthLabel.vue';
+
+const props = defineProps<{
+  modelValue: string;
+  options: Option[];
+  disabled?: boolean;
+  label?: string;
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void;
+}>();
+
+const id = computed(() => `select-${Math.random().toString().substring(2)}`);
+
+const selectedOption = computed<Option>(() => {
+  return props.options.find(
+    (option) => option.value === props.modelValue,
+  ) as Option;
+});
+
+function handleUpdate(value: string): void {
+  emit('update:modelValue', value);
+}
+</script>
+
+<script lang="ts">
+interface Option {
+  value: string;
+  label: string;
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { Option };
+</script>
+
+<style scoped>
+.radio {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-small);
+}
+
+.list {
+  display: flex;
+  gap: var(--spacing-normal);
+}
+
+.caption {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  padding: 0;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  border-width: 0;
+  white-space: nowrap;
+}
+
+.option {
+  display: flex;
+  position: relative;
+  padding: 8px;
+  border: 1px solid var(--color-border-primary);
+  border-radius: var(--border-radius-big);
+  outline: none;
+  cursor: pointer;
+}
+
+.option.active {
+  border-color: var(--color-border-secondary);
+}
+
+.option.checked {
+  background: var(--color-bg-secondary);
+}
+
+.label {
+  margin: 0;
+  color: var(--color-text-secondary);
+}
+
+.label.checked {
+  color: var(--color-text-primary);
+}
+</style>
