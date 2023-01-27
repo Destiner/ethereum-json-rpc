a0a1f298d47481fd3c690d2b9271e8dc8d175633
Timur Badretdinov <destinerx@gmail.com>
2023-01-28 00:03:16 +0400

    Add Cross icon component

---
+++ src/components/__common/icon/Cross.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M18 6L6 18M6 6l12 12"
+    />
+  </svg>
+</template>
