9b73b015ab750388ccb85fac7b9f4420c7e4da5a
Timur Badretdinov <destinerx@gmail.com>
2023-06-11 01:02:23 +0400

    Add formatters util

---
+++ src/utils/formatters.ts
@@ -0,0 +1,29 @@
+import { Tier, Feature } from './providers';
+
+function formatProviderTier(tier: Tier): string {
+  switch (tier) {
+    case 'no_key':
+      return 'No Key';
+    case 'free_key':
+      return 'Free Key';
+    case 'paid_key':
+      return 'Paid Key';
+  }
+}
+
+function formatProviderFeature(feature: Feature): string {
+  switch (feature) {
+    case 'websocket':
+      return 'WS';
+    case 'archive_node':
+      return 'Archive Data';
+    case 'debug':
+      return 'Debug Methods';
+    case 'traces':
+      return 'Trace Methods';
+    case 'erigon':
+      return 'Erigon Node';
+  }
+}
+
+export { formatProviderFeature, formatProviderTier };
