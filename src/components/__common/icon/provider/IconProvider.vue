a21b808978f5cd148c333cfaadb547c56981d09e
Timur Badretdinov <destinerx@gmail.com>
2023-06-11 01:02:44 +0400

    Add provider icons

---
+++ src/components/__common/icon/provider/IconProvider.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <IconAlchemy v-if="provider === ALCHEMY" />
+    <IconAnkr v-if="provider === ANKR" />
+    <IconBlast v-if="provider === BLAST" />
+    <IconCloudflare v-if="provider === CLOUDFLARE" />
+    <IconInfura v-if="provider === INFURA" />
+    <IconLlama v-if="provider === LLAMA" />
+    <IconPokt v-if="provider === POKT" />
+    <IconQuicknode v-if="provider === QUICKNODE" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  ALCHEMY,
+  ANKR,
+  BLAST,
+  CLOUDFLARE,
+  INFURA,
+  LLAMA,
+  POKT,
+  QUICKNODE,
+  Provider,
+} from '@/utils/providers';
+
+import IconAlchemy from './Alchemy.vue';
+import IconAnkr from './Ankr.vue';
+import IconBlast from './Blast.vue';
+import IconCloudflare from './Cloudflare.vue';
+import IconInfura from './Infura.vue';
+import IconLlama from './Llama.vue';
+import IconPokt from './Pokt.vue';
+import IconQuicknode from './Quicknode.vue';
+
+defineProps<{
+  provider: Provider;
+}>();
+</script>
