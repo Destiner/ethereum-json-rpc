63124437d68d657518649c73f43fdcfd2ada46f5
Timur Badretdinov <destinerx@gmail.com>
2023-06-11 01:02:03 +0400

    Chain icons

---
+++ src/components/__common/icon/chain/IconChain.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ArbitrumIcon v-if="chain === ARBITRUM" />
+    <EthereumIcon v-if="chain === ETHEREUM" />
+    <OptimismIcon v-if="chain === OPTIMISM" />
+    <PolygonIcon v-if="chain === POLYGON" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ETHEREUM, ARBITRUM, OPTIMISM, POLYGON, Chain } from '@/utils/chains';
+
+import ArbitrumIcon from './Arbitrum.vue';
+import EthereumIcon from './Ethereum.vue';
+import OptimismIcon from './Optimism.vue';
+import PolygonIcon from './Polygon.vue';
+
+defineProps<{
+  chain: Chain;
+}>();
+</script>
