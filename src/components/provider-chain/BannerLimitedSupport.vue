a294b049df0ae77b95ab5eacd46607109eac542a
Timur Badretdinov <destinerx@gmail.com>
2023-06-18 13:05:18 +0400

    Add limited support banner component

---
+++ src/components/provider-chain/BannerLimitedSupport.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="banner">
+    At the moment, all profiling and feature support evaluation is done on
+    Ethereum only using free/no API keys.
+  </div>
+</template>
+
+<style scoped>
+.banner {
+  padding: 8px;
+  border: 1px solid var(--color-border-info);
+  border-radius: var(--border-radius-medium);
+  background: var(--color-bg-info);
+  color: var(--color-text-info);
+  font-size: var(--font-size-normal);
+}
+</style>
