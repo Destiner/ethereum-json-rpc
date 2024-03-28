23f931d503a41b4abeccdc68e11725a2bbc6e553
Timur Badretdinov <accounts@destiner.io>
2024-03-28 03:34:25 +0000

    Use radix-vue for EthToggle

--- src/components/__common/EthToggle.vue
+++ src/components/__common/EthToggle.vue
@@ -1,28 +1,19 @@
 <template>
-  <div class="toggle">
-    <Switch
-      v-slot="{ checked }"
+  <div class="switch">
+    <Switch.Root
       :model-value="modelValue"
       as="template"
       @update:model-value="handleUpdate"
     >
-      <button
-        class="switch"
-        :class="{ active: checked }"
-      >
-        <span class="caption">Use setting</span>
-        <span
-          aria-hidden="true"
-          :class="{ active: checked }"
-          class="trigger"
-        />
+      <button class="root">
+        <Switch.Thumb class="thumb" />
       </button>
-    </Switch>
+    </Switch.Root>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Switch } from '@headlessui/vue';
+import { Switch } from 'radix-vue/namespaced';
 
 defineProps<{
   modelValue: boolean;
@@ -44,18 +35,51 @@ function handleUpdate(value: boolean): void {
   --travel-length: 20px;
 }
 
-.caption {
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  margin: -1px;
+.root {
+  display: inline-flex;
+  position: relative;
+  width: calc(
+    var(--trigger-size) + var(--travel-length) + 2 * var(--border-width)
+  );
+  height: calc(var(--trigger-size) + 2 * var(--border-width));
   padding: 0;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  border-width: 0;
-  white-space: nowrap;
+  transition: color 0.2s;
+  transition-duration: 200ms;
+  transition-timing-function: ease-in-out;
+  border-width: var(--border-width);
+  border-radius: calc((var(--trigger-size) + 2 * var(--border-width)) / 2);
+  border-color: transparent;
+  outline: none;
+  outline-color: #ffffffbf;
+  outline-width: var(--border-width);
+  background: var(--color-bg-tertiary);
+  cursor: pointer;
 }
 
+.root[data-state='checked'] {
+  background: var(--color-accent-secondary);
+}
+
+.thumb {
+  display: inline-block;
+  width: var(--trigger-size);
+  height: var(--trigger-size);
+  transform: translateX(0);
+  transition: all 0.2s ease-in-out;
+  border-radius: 100%;
+  outline: 0;
+  background-color: white;
+  box-shadow:
+    0 4px 6px #0000001a,
+    0 2px 4px #0000000f;
+  pointer-events: none;
+}
+
+.thumb[data-state='checked'] {
+  transform: translateX(var(--travel-length));
+}
+
+/* 
 button {
   display: inline-flex;
   position: relative;
@@ -98,5 +122,5 @@ button.active {
 
 .trigger.active {
   transform: translateX(var(--travel-length));
-}
+} */
 </style>
